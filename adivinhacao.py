36642a49c859f8d6c38bbe288edaf739cf9a5477
henriqy <henrisoft3@gmail.com>
2023-06-07 16:10:08 -0300

    laco repeticao while

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,22 +3,29 @@ print("bem vindo ao jogo de adivinhacao")
 print("********************************")
 
 numero_secreto = 42
+total_de_tetantivas = 3
+rodada = 1
 
-chute_str = input("Digite o seu numero: ")
+while(rodada <= total_de_tetantivas):
+    print("tentativa", rodada,"de", total_de_tetantivas)
 
-chute = int(chute_str)
+    chute_str = input("Digite o seu numero: ")
 
-acertou = (numero_secreto == chute)
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    chute = int(chute_str)
 
-if(numero_secreto == chute):
-    print("voce acertou")
-else:
-    if(maior):
-       print("voce errou, o seu chute foi maior que o numero secreto")
-    elif(menor):
-       print("Voce errou, seu chute foi menor que o numero secreto")
+    acertou = (numero_secreto == chute)
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if(numero_secreto == chute):
+        print("voce acertou")
+    else:
+        if(maior):
+            print("voce errou, o seu chute foi maior que o numero secreto")
+
+        elif(menor):
+            print("Voce errou, seu chute foi menor que o numero secreto")
+        
+    print("Fim do Jogo")
     
-print("Fim do Jogo")
-    
+    rodada = rodada + 1
