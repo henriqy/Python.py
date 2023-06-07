39374c20866f8c4bcc235ebac9dd1c5555815c16
henriqy <henrisoft3@gmail.com>
2023-06-07 11:03:30 -0300

    conhecendo a funcao elif

--- adivinhacao.py
+++ adivinhacao.py
@@ -8,8 +8,17 @@ chute_str = input("Digite o seu numero: ")
 
 chute = int(chute_str)
 
+acertou = (numero_secreto == chute)
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
 if(numero_secreto == chute):
     print("voce acertou")
 else:
-    print("voce errou")
+    if(maior):
+       print("voce errou, o seu chute foi maior que o numero secreto")
+    elif(menor):
+       print("Voce errou, seu chute foi menor que o numero secreto")
+    
+print("Fim do Jogo")
     
