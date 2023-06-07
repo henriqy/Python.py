903215bd29d05ca5feac6cb7669e7953abb53160
henriqy <henrisoft3@gmail.com>
2023-06-07 19:21:45 -0300

    break, continue, or, and

--- adivinhacao.py
+++ adivinhacao.py
@@ -13,19 +13,25 @@ for rodada in range (1,total_de_tetantivas + 1):
 
     chute = int(chute_str)
 
+    if(chute < 1 or chute > 100):
+        print("Nao pode chutar numeros menores que 1 ou maiores que 100")
+        continue
+
     acertou = (numero_secreto == chute)
     maior = chute > numero_secreto
     menor = chute < numero_secreto
 
     if(numero_secreto == chute):
         print("voce acertou")
+        break
     else:
         if(maior):
             print("voce errou, o seu chute foi maior que o numero secreto")
 
         elif(menor):
             print("Voce errou, seu chute foi menor que o numero secreto")
-        
-    print("Fim do Jogo")
     
     rodada = rodada + 1
+    
+    print("Fim do Jogo")
+    
