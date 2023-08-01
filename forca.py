ef7159161f98dc28e19d1ae1ff1dc7e0b8e9182d
henriqy <henrisoft3@gmail.com>
2023-08-01 15:21:33 -0300

    percorrendo a string, uso do for

--- forca.py
+++ forca.py
@@ -4,6 +4,22 @@ def jogar():
     print("bem vindo ao jogo de forca")
     print("********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not acertou and not enforcou):
+        chute = input("Qual letra?")
+        
+        index = 0
+
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Encontrei a letra {} na posicao {}".format(letra, index))
+            index = index +1
+
+
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
