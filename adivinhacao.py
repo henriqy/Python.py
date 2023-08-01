aae166b4278dd1e3bdb420e0de89b9cc06056df6
henriqy <henrisoft3@gmail.com>
2023-07-31 22:23:39 -0300

    diferenciando um arquivo importado e um executado

--- adivinhacao.py
+++ adivinhacao.py
@@ -96,4 +96,6 @@ def jogar():
         rodada = rodada + 1
         
 
+if(__name__ == "__main__"):
+    jogar()
         
--- jogos.py
+++ jogos.py
@@ -1,16 +1,21 @@
 import forca
 import adivinhacao
 
-print("********************************")
-print("*******Escolha o seu jogo*******")
-print("********************************")
+def escolhe_jogo():
 
-print("(1) forca (2) Advinhacao")
+    print("********************************")
+    print("*******Escolha o seu jogo*******")
+    print("********************************")
 
-jogo = int(input("Qual jogo?"))
-if(jogo == 1):
-    print("jogando forca")
-    forca.jogar()
-elif(jogo == 2):
-    print("Jogando advinhacao")
-    adivinhacao.jogar()
+    print("(1) forca (2) Advinhacao")
+
+    jogo = int(input("Qual jogo?"))
+    if(jogo == 1):
+        print("jogando forca")
+        forca.jogar()
+    elif(jogo == 2):
+        print("Jogando advinhacao")
+        adivinhacao.jogar()
+
+if(__nome__ == "__main__")
+    escolhe_jogo()
