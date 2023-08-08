f60c8857ac537be29537cd0be631779aa74dfb32
henriqy <henrisoft3@gmail.com>
2023-08-07 22:52:14 -0300

    saindo do laco e skills

--- forca.py
+++ forca.py
@@ -4,21 +4,39 @@ def jogar():
     print("bem vindo ao jogo de forca")
     print("********************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
+    erros = 0
 
     while(not acertou and not enforcou):
         chute = input("Qual letra?")
-        chute = chute.strip()
+        chute = chute.strip().upper()
         index = 0
 
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index +1
+        if(chute in palavra_secreta):
+
+            for letra in palavra_secreta:
+                if(chute.upper() == letra.upper()):
+                    letras_acertadas[index] = letra
+                index = index +1
+            else:
+                erros += 1
+
+            enforcou = erros == 6
+            acertou = "_" not in letras_acertadas
+            print(letras_acertadas)
+
+            if(acertou):
+                print("voce ganhou")
+            else:
+                print("voce perdeu")
+            
+
+
+      
 
         print(letras_acertadas) 
 
