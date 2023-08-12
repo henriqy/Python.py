e5c7b47ef901f884944918d59a116888d731dedf
henriqy <henrisoft3@gmail.com>
2023-08-12 19:54:03 -0300

    metodos saca e deposita

--- conta.py
+++ conta.py
@@ -1,7 +1,19 @@
 class Conta:
+    
     def __init__(self, numero, titular, saldo, limite):
        print("construindo objeto...")
        self.numero = numero
        self.titular = titular
        self.saldo = saldo
        self.limite = limite
+
+    def estrato(self):
+        print("Saldo de {} do titular {}".format(self.saldo,self.titular))
+    
+    def deposita(self,valor):
+        self.saldo += valor
+    
+    def saca(self, valor):
+        self.saldo -= valor
+    
+    
