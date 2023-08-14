5ffbaee72ed62a94517e256623d03c21b1530279
henriqy <henrisoft3@gmail.com>
2023-08-14 12:02:28 -0300

    estudo do get e set, e suas propriedades

--- conta.py
+++ conta.py
@@ -15,5 +15,31 @@ class Conta:
     
     def saca(self, valor):
         self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
     
+    
+    def get_saldo(self):
+        return self.__saldo
+    
+    def set_saldo(self, saldo):
+        self.__saldo = saldo
+    
+    def get_titular(self):
+        return self.__titular
+    
+    def set_titular(self, titular):
+        self.__titular = titular
+    
+    @property
+    def limite(self):
+        return self.__limite
+    
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+    
+
     
